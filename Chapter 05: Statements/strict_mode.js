a142e1e4d3ab6d5eb178fa5ea3e968236fe21fff
Manish Goyal <manishgoyal5683@gmail.com>
2026-04-25 00:35:31 +0530

    ch 05

--- Chapter 05: Statements/strict_mode.js
+++ Chapter 05: Statements/strict_mode.js
@@ -1,4 +1,17 @@
 "use strict";
 
+//01. Can't use "let":
 let = 0;
 console.log(let); //error
+
+
+//02. Can't implicitly define properties to the global object:
+x = 10;
+console.log(x); //error
+
+
+//03. "this" will be undefined (not global):
+function fn()
+{
+    console.log(this); //undefined
+}
